a9a6f795776450dbe113425444b355b5100610bb
Sara1705722 <91327678+Sara1705722@users.noreply.github.com>
2021-11-26 20:26:16 +0300

    Update Payment.java

--- Payment.java
+++ Payment.java
@@ -1,57 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pet;
 
-import java.util.Date;
 
-/**
- *
- * @author Lenovo
- */
+
 public class Payment {
 
-private int cardNo;
-private int cvcNo;
-private Date expDate;
-private Order orders;
 
-public Payment(int cardNo, int cvcNo, Date expDate, Order orders) {
+    private String cardNo;  // Creadit Card Number
+    private String cvvNo;   // cvv Number
+    private String expDate;
+ 
+   public payment() {
+
+    }
+
+   public Payment(String cardNo, String cvcNo, String expDate) {
         this.cardNo = cardNo;
         this.cvcNo = cvcNo;
         this.expDate = expDate;
-        this.orders = orders;
+       
     }
 
 
-    public int getCardNo() {
+    public String getCardNo() {
         return cardNo;
     }
 
-    public void setCardNo(int cardNo) {
+    public void setCardNo(String cardNo) {
         this.cardNo = cardNo;
     }
 
-    public int getCvcNo() {
+    public String getCvcNo() {
         return cvcNo;
     }
 
-    public void setCvcNo(int cvcNo) {
+    public void setCvcNo(String cvcNo) {
         this.cvcNo = cvcNo;
     }
 
-    public Date getExpDate() {
+    public String getExpDate() {
         return expDate;
     }
 
-    public void setExpDate(Date expDate) {
+    public void setExpDate(String expDate) {
         this.expDate = expDate;
     }
 
+ public void updateCeditCardInfo(String cardNo, String cvvNo, String expDate) {
+        this.cardNo = cardNo;
+        this.cvvNo = cvvNo;
+        this.expDate = expDate;
 
+    }
+        
+         // checking if the input from user is numerical
+     public boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+        
+
+         //checking if the max card number is 16
+    public boolean checkCardNo(String cardNo) {
+        if (cardNo.length() == 16) {
+            if (isNumeric(cardNo)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+        
+
+    //checking if the max cvv number is 3
+     public boolean checkCVV(String cvv) {
+        if (cvv.length() ==3) {
+            if (isNumeric(cvv)) {
+                return true;
+            }
+        }
+        return false;
+    }
+        
     
+         //checking if the max date(year&month) number is 3
+  public boolean checkExpDate(String month ,String year) {
+        if (month.length() < 3 && year.length() < 3 ) {
+            
+            if (isNumeric(month)&&isNumeric(year)) {
+                    
+                if(Integer.parseInt(month)< 13 && Integer.parseInt(year)>=21){
+                return true;
+              
+             
+                }
+            }
+        }
+        return false;
+    }
+
 
 
    
