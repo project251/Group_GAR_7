760d91ad31fc8fcf0aaf3abefc5c0ac0efc6d0a1
Aljoharahz <91691239+Aljoharahz@users.noreply.github.com>
2021-11-29 10:27:01 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pet;
 
 import java.util.Date;
@@ -10,18 +6,18 @@ import java.util.Date;
 
 public class Order {
     
- private String orderId ;
- private Date orderDate ;
- private Date shippingDate ;
+ private String orderId ;   
  private int shippingId ;
- private String status ;
-  private String customerFirstName ;
- private String customerLastName ;
 
+    
    // array to store the products 
     List<Product> products = new ArrayList<Product>();
 
+ public order(String orderId, int shippingId){
  
+ this.orderId=orderId;
+     this.shippingId=shippingId;
+ }
     public String getOrderId() {
         return orderId;
     }
@@ -30,21 +26,7 @@ public class Order {
         this.orderId = orderId;
     }
 
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public Date getShippingDate() {
-        return shippingDate;
-    }
-
-    public void setShippingDate(Date shippingDate) {
-        this.shippingDate = shippingDate;
-    }
+   
 
     public int getShippingId() {
         return shippingId;
@@ -54,30 +36,6 @@ public class Order {
         this.shippingId = shippingId;
     }
 
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getCustomerFirstName() {
-        return customerFirstName;
-    }
-
-    public void setCustomerFirstName(String customerFirstName) {
-        this.customerFirstName = customerFirstName;
-    }
-
-    public String getCustomerLastName() {
-        return customerLastName;
-    }
-
-    public void setCustomerLastName(String customerLastName) {
-        this.customerLastName = customerLastName;
-    }
- 
 
 
  
